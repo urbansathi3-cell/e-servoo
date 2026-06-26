a509f34afbd6a99e34034d322fcb67722c3f3f47
urbansathi3-cell <urbansathi3@gmail.com>
2026-06-26 17:41:55 +0530

    Update WhatsappButton.jsx

--- src/components/WhatsappButton.jsx
+++ src/components/WhatsappButton.jsx
@@ -4,7 +4,7 @@ function WhatsappButton() {
       href="https://wa.me/919583896129"
       target="_blank"
       rel="noopener noreferrer"
-      className="fixed bottom-28 right-5 z-50 bg-green-500 text-white px-5 py-3 rounded-full shadow-lg hover:bg-green-600"
+      className="fixed bottom-24 right-5 z-50 bg-green-500 text-white px-5 py-3 rounded-full shadow-lg hover:bg-green-600"
     >
       WhatsApp
     </a>
