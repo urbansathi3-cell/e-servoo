6ed8b14a5174727055fc785edcc20d6027643759
urbansathi3-cell <urbansathi3@gmail.com>
2026-06-30 08:24:49 +0530

    Update Stats.jsx

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -41,38 +41,48 @@ function Stats() {
         <div className="grid grid-cols-2 md:grid-cols-5 gap-5">
 
           <div className="bg-[#6A9A9A] p-5 rounded-2xl text-center shadow-md">
-            <h2 className="text-3xl font-bold text-blue-400">
+            <h2 className="text-3xl font-bold text-[#083D4A]">
               {stats.workers}
             </h2>
-            <p>Workers</p>
+            <p className="text-[#E1E9E5] font-medium">
+  Workers
+</p>
           </div>
 
           <div className="bg-[#6A9A9A] p-5 rounded-2xl text-center shadow-md">
-            <h2 className="text-3xl font-bold text-green-400">
+            <h2 className="text-3xl font-bold text-[#0B4F45]">
               {stats.services}
             </h2>
-            <p>Services</p>
+            <p className="text-[#E1E9E5] font-medium">
+  Services
+</p>
           </div>
 
           <div className="bg-[#6A9A9A] p-5 rounded-2xl text-center shadow-md">
-            <h2 className="text-3xl font-bold text-yellow-400">
+            <h2 className="text-3xl font-bold text-[#7A5A00]">
               {stats.bookings}
             </h2>
-            <p>Bookings</p>
+            <p className="text-[#E1E9E5] font-medium">
+  Bookings
+</p>
           </div>
 
           <div className="bg-[#6A9A9A] p-5 rounded-2xl text-center shadow-md">
-            <h2 className="text-3xl font-bold text-purple-400">
+            <h2 className="text-3xl font-bold text-[#4E3D7A]">
               {stats.completedJobs}
             </h2>
-            <p>Completed Jobs</p>
+            <p className="text-[#E1E9E5] font-medium">
+  Completed Jobs
+</p>
           </div>
 
           <div className="bg-[#6A9A9A] p-5 rounded-2xl text-center shadow-md">
             <h2 className="text-3xl font-bold text-orange-400">
               {stats.satisfaction}
             </h2>
-            <p>Satisfaction</p>
+            <p className="text-[#E1E9E5] font-medium">
+  Satisfaction
+</p>
           </div>
 
         </div>
